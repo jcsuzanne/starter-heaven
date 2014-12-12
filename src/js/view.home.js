855dd674118dfd723152e86d0e56c8729cf5f558
JCS PRO <JCS-PRO@iMac-de-JCS.local>
2014-12-12 17:34:50 +0100

    update home JS

--- src/js/view.home.js
+++ src/js/view.home.js
@@ -4,14 +4,26 @@
     SITE.home = (function()
     {
 
+        var DOM = function(_view)
+        {
+            this.view           =   (typeof _view != 'undefined')?_view.find('.layout__home'):$('.layout__home');
+            this.init();
+        }
+        DOM.prototype = {
+            init : function()
+            {
+
+            }
+        }
+
         var init = function()
         {
-
+            new DOM();
         }
 
         var after = function(_view)
         {
-
+            new DOM(_view);
         }
 
         return {
