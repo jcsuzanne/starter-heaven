1dae2bdd5a8d01de9a387fb45123fd1c8da65eba
JCS PRO <JCS-PRO@iMac-de-JCS.local>
2014-12-12 17:03:06 +0100

    add home JS

---
+++ src/js/view.home.js
@@ -0,0 +1,17 @@
+(function(window, document, undefined) {
+    'use strict';
+
+    SITE.home = (function()
+    {
+
+        var init = function()
+        {
+
+        }
+
+        return {
+                init : init
+        }
+    })();
+
+}(window, document));
